a3024dccfc44a1fd489d0e7173b47f475f1f3f75
Enrico Marino <onirame@gmail.com>
2011-11-06 17:02:08 +0100

    crumbs

--- contemplate.js
+++ contemplate.js
@@ -1,9 +1,9 @@
 // inspired by Microtemplatez - Copyright 2011 Addy Osmani
 
-var contemplate = function (window, undef) {
+var contemplate = function (context, undef) {
 
-    return function( tmpl, data ) {
-        return tmpl.replace((RegExp("{{\\s*([a-z0-9_][\\.a-z0-9_]*)\\s*}}", "gi")), function (tag, k) {
+    return function (tmpl, data) {
+        return tmpl.replace(/{{\s*([a-z0-9_][\.a-z0-9_]*)\s*}}/gi, function (tag, k) {
             for (var p = k.split("."), len = p.length, i = 0, temp = data; i < len; i += 1) {
                 temp = temp[p[i]];
             }
