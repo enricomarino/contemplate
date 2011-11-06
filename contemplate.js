698058194ec390dc4d6da3576b524e2ebf5eda36
Enrico Marino <onirame@gmail.com>
2011-11-06 16:53:50 +0100

    added contemplate

---
+++ contemplate.js
@@ -0,0 +1,14 @@
+// inspired by Microtemplatez - Copyright 2011 Addy Osmani
+
+var contemplate = function (window, undef) {
+
+    return function( tmpl, data ) {
+        return tmpl.replace((RegExp("{{\\s*([a-z0-9_][\\.a-z0-9_]*)\\s*}}", "gi")), function (tag, k) {
+            for (var p = k.split("."), len = p.length, i = 0, temp = data; i < len; i += 1) {
+                temp = temp[p[i]];
+            }
+            return temp;
+        });
+    };
+
+}(this));
